3e971f059ffcc9c6c371320044753cbf372b1177
PTSoup <phaimani.souphaphone@gmail.com>
2018-11-11 18:18:14 -0600

    Complete rendering App component to the DOM

---
+++ src/index-compiled.js
@@ -0,0 +1,10 @@
+// TODO: Render the `App` component to the DOM
+var ninja = '<div>ninja</div>';
+
+var run = function () {
+  $(document).ready(function () {
+    $('#app').append('<div>ninja</div>');
+  });
+};
+
+run;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 // TODO: Render the `App` component to the DOM
+var Homepage = React.createClass({
+  render: function() {
+    return ( <App />);
+  }
+});
 
+ReactDOM.render( <Homepage />, document.getElementById('app'));
